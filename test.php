e9820a83cfad60e9513d18376fa0385502d3c857
achmadszm <achmadsz9222@gmail.com>
2019-08-06 18:05:20 +0700

    test6

--- test.php
+++ test.php
@@ -4,7 +4,71 @@
 	<title>Testing PHP</title>
 </head>
 <body>
-	 <form action="" method="POST" enctype="multipart/form-data">
+<script type="text/javascript">
+        function processImage(urlFile) {
+            // **********************************************
+            // *** Update or verify the following values. ***
+            // **********************************************
+    
+            // Replace <Subscription Key> with your valid subscription key.
+            var subscriptionKey = "d7b49d561ef342f6be3214ba8bcbee47";
+    
+            // You must use the same Azure region in your REST API method as you used to
+            // get your subscription keys. For example, if you got your subscription keys
+            // from the West US region, replace "westcentralus" in the URL
+            // below with "westus".
+            //
+            // Free trial subscription keys are generated in the "westus" region.
+            // If you use a free trial subscription key, you shouldn't need to change
+            // this region.
+            var uriBase =
+                "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+    
+            // Request parameters.
+            var params = {
+                "visualFeatures": "Categories,Description,Color",
+                "details": "",
+                "language": "en",
+            };
+    
+            // Display the image.
+            document.querySelector("#sourceImage").src = urlFile;
+    
+            // Make the REST API call.
+            $.ajax({
+                url: uriBase + "?" + $.param(params),
+    
+                // Request headers.
+                beforeSend: function(xhrObj){
+                    xhrObj.setRequestHeader("Content-Type","application/json");
+                    xhrObj.setRequestHeader(
+                        "Ocp-Apim-Subscription-Key", subscriptionKey);
+                },
+    
+                type: "POST",
+    
+                // Request body.
+                data: '{"url": ' + '"' + urlFile + '"}',
+            })
+    
+            .done(function(data) {
+                // Show formatted JSON on webpage.
+               //$("#responseTextArea").val(JSON.stringify(data, null, 2));
+
+               $("#responseTextArea").val(JSON.stringify(data["description"]["captions"][0].text,null,2));
+            })
+    
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ? "Error. " :
+                    errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ? "" :
+                    jQuery.parseJSON(jqXHR.responseText).message;
+                alert(errorString);
+            });
+        };
+    </script>
+	 <form action="process.php" method="POST" enctype="multipart/form-data">
                 <div>Pilih gambar : <input type="file" name="fileToUpload" ></div>
                 <br>
                 <div>
@@ -13,20 +77,5 @@
                     <input type="submit" name="Clear" value="Clear ALL Data" class="button">
                 <div>
             </form>
-			<?php 
-					if($_GET){
-                    if (isset($_POST['Upload'])) {
-						echo "No button press Upload.";
-                    }elseif(isset($_POST['Clear'])) {
-						echo "No button press Clear.";
-                    }elseif(isset($_POST['ShowData'])) {
-						echo "No button press ShowData.";
-					}
-				}else{
-					echo "No button press.";
-				}
-                    
- 
-                ?>
 </body>
 </htmL>
